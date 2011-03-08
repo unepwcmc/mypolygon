6a0184b6ac78f08a337dc536255b38431506ab58
André Neves <Andre.Neves@unep-wcmc.org>
2011-03-08 18:14:21 +0000

    Some changes preparing for edition of uploaded pg.

--- app/controllers/assesments_controller.rb
+++ app/controllers/assesments_controller.rb
@@ -1,78 +1,86 @@
 class AssesmentsController < ApplicationController
   layout 'base_layout', :except => 'index'
   
-  def index 
+  def index
     if !session[:assesment_ids].blank?
       @assesments = Assesment.find(session[:assesment_ids], :order => "created_at DESC")
     else  
       @assesments ||= []
-    end 
-    @assesment = Assesment.new
+    end
+
+    if session[:assesment_to_preload]
+      @assesment = Assesment.find(session[:assesment_to_preload])
+      session[:assesment_to_preload] = nil
+    else
+      @assesment = Assesment.new
+    end
   end
   
   
   def create
     begin
-    file_name = params[:assesment][:shape].original_filename
-    
-    #test file ending    
-    if !file_name.ends_with? ".zip" 
-      flash[:notice] = "You must import a zip file"
-      redirect_to root_url
-      return
-    end
-        
-    #GENERATE INDIVIDUAL FOLDER
-    directory_name = Digest::SHA1.hexdigest("#{Time.now.usec}#{file_name}")
-    directory = "#{Rails.root}/tmp/shape_uploads/#{directory_name}"
-                                
-    #BUILD FOLDER IF NOT THERE
-    FileUtils.mkdir_p directory
-    
-    #create the total file path to save file
-    path = File.join(directory, file_name)
-        
-    # write the file
-    File.open(path, "wb") { |f| f.write(params[:assesment][:shape].read) }
-    
-    #Crack open the zip
-    Zip::ZipFile.open(path) do |zip_file|
-      zip_file.each do |f|
-        real_file_name = f.name.split("/").last
-        file_path = File.join(directory, real_file_name)
-        zip_file.extract(f, file_path)
-      end
-    end
+      file_name = params[:assesment][:shape].original_filename
 
-    #open the shape files and read em in
-    Dir.new(directory).each do |f|
-      if f.ends_with? ".shp"
-        #CREATE ASSESSMENT
-        @assesment ||= Assesment.create(:file_name => file_name)
-        session[:assesment_ids] ||= []
-        session[:assesment_ids] << @assesment.id
-        
-        #READ IN AND CREATE TENEMENTS
-        GeoRuby::Shp4r::ShpFile.open(File.join(directory,f)) do |shp|
-          shp.each do |shape|
-            @assesment.tenements.create :the_geom => shape.geometry, :attribute_data => shape.data
-          end        
+      #test file ending
+      if !file_name.ends_with? ".zip"
+        flash[:notice] = "You must import a zip file"
+        redirect_to root_url
+        return
+      end
+
+      #GENERATE INDIVIDUAL FOLDER
+      directory_name = Digest::SHA1.hexdigest("#{Time.now.usec}#{file_name}")
+      directory = "#{Rails.root}/tmp/shape_uploads/#{directory_name}"
+
+      #BUILD FOLDER IF NOT THERE
+      FileUtils.mkdir_p directory
+
+      #create the total file path to save file
+      path = File.join(directory, file_name)
+
+      # write the file
+      File.open(path, "wb") { |f| f.write(params[:assesment][:shape].read) }
+
+      #Crack open the zip
+      Zip::ZipFile.open(path) do |zip_file|
+        zip_file.each do |f|
+          real_file_name = f.name.split("/").last
+          file_path = File.join(directory, real_file_name)
+          zip_file.extract(f, file_path)
         end
       end
-    end
-    
-    #WE WOULD NORMALLY ADD TO USER HERE, BUT TEMP IS ADD TO SESSION
-    
-    #cleanup files
-    FileUtils.rm_rf directory
-    
-    # Query API
-    @assesment.analyse
-    
-    if @assesment
-      flash[:notice] = "analysis complete"
-    end
-      redirect_to assesment_path(@assesment)      
+
+      #open the shape files and read em in
+      Dir.new(directory).each do |f|
+        if f.ends_with? ".shp"
+          #CREATE ASSESSMENT
+          @assesment ||= Assesment.create(:file_name => file_name)
+          session[:assesment_ids] ||= []
+          session[:assesment_ids] << @assesment.id
+
+          #READ IN AND CREATE TENEMENTS
+          GeoRuby::Shp4r::ShpFile.open(File.join(directory,f)) do |shp|
+            shp.each do |shape|
+              @assesment.tenements.create :the_geom => shape.geometry, :attribute_data => shape.data
+            end
+          end
+        end
+      end
+
+      #WE WOULD NORMALLY ADD TO USER HERE, BUT TEMP IS ADD TO SESSION
+
+      #cleanup files
+      FileUtils.rm_rf directory
+
+      # Query API
+      @assesment.analyse
+
+      if @assesment
+        flash[:notice] = "analysis complete"
+      end
+      session[:assesment_to_preload] = @assesment.id
+      redirect_to assesment_path(@assesment)
+
     rescue Exception => e
       msg = "File couldn't be uploaded: #{e.inspect}"
       Rails.logger.fatal msg
