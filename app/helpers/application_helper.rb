ba144124ca7083210fd58d68a65e2376c0b2c597
André Neves <Andre.Neves@unep-wcmc.org>
2011-03-22 17:44:01 +0000

    better visualization of data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,7 @@ module ApplicationHelper
     "environments/#{Rails.env.downcase}"
   end
 
-  def to_percent amount, total, precision=1, none_text = 'none'
-    percent = amount.out_of total
+  def to_percent percent, precision=1, none_text = 'none'
     return none_text if percent == 0
     percent = number_with_precision(100 * percent, :precision => precision)
     percent.to_s + '%'
