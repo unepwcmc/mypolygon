e814f63cda1f8d76d5f9c04387d80a466bdb1add
André Neves <Andre.Neves@unep-wcmc.org>
2011-03-22 19:25:59 +0000

    Percents with bars. Polygon with coral coverage.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,19 @@ module ApplicationHelper
   def to_percent percent, precision=1, none_text = 'none'
     return none_text if percent == 0
     percent = number_with_precision(100 * percent, :precision => precision)
-    percent.to_s + '%'
+    percent_bar(percent) + percent.to_s + '%'
+  end
+
+  def percent_bar percent
+    " <span class='percent'><div class='bar' style='width: #{percent}%; background-color: #{calculate_bg percent};'></div>#{percent} %</span> ".html_safe
+  end
+
+  private
+  def calculate_bg percent
+    percent = percent.to_f unless percent.is_a? Fixnum
+    red = percent*2.55#/100
+    green = (100-percent)*2.55#/100
+    blue = 0
+    "#%02x%02x%02x" % [red, green, blue]
   end
 end
