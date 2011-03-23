08f2062eeacc60aedd55e972c9a31fec8f597fa5
André Neves <Andre.Neves@unep-wcmc.org>
2011-03-23 19:19:04 +0000

    percents with slider

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,15 @@ module ApplicationHelper
   def to_percent percent, precision=1, none_text = 'none'
     return none_text if percent == 0
     percent = number_with_precision(100 * percent, :precision => precision)
-    percent_bar(percent) + percent.to_s + '%'
+    percent_slider(percent) + percent.to_s + '%'
   end
 
   def percent_bar percent
     " <span class='percent'><div class='bar' style='width: #{percent}%; background-color: #{calculate_bg percent};'></div>#{percent} %</span> ".html_safe
+  end
+
+  def percent_slider percent
+    " <span class='percent_slider'><div class='bar' style='left: #{percent}%; background-color: #{calculate_bg percent};'></div>#{percent} %</span> ".html_safe
   end
 
   private
