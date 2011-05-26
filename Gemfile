ffdbe8158fa6715cd2c768abc16a0e4e05e4ace6
Simão Belchior de Castro <simaobelchior@gmail.com>
2011-05-26 12:18:17 +0100

    Brightboxified the project

--- Gemfile
+++ Gemfile
@@ -22,12 +22,6 @@ gem "googlecharts", :require => "gchart"
 # Use unicorn as the web server
 # gem 'unicorn'
 
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
-# gem 'ruby-debug'
-gem 'ruby-debug19', :require => 'ruby-debug'
 
 # Bundle the extra gems:
 # gem 'bj'
@@ -38,6 +32,13 @@ gem 'ruby-debug19', :require => 'ruby-debug'
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
-# group :development, :test do
-#   gem 'webrat'
-# end
+group :development, :test do
+  #   gem 'webrat'
+  # Deploy with Capistrano
+  gem 'capistrano'
+  gem 'capistrano-ext'
+  
+  # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
+  # gem 'ruby-debug'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,89 @@
+## Generated with 'brightbox' on 2011-05-11 16:48:20 +0100
+gem 'brightbox', '>=2.3.8'
+require 'brightbox/recipes'
+require 'brightbox/passenger'
+require 'bundler/capistrano'
+
+# The name of your application.  Used for deployment directory and filenames
+# and Apache configs. Should be unique on the Brightbox
+set :application, "mypolygon"
+
+# Primary domain name of your application. Used in the Apache configs
+set :domain, "unepwcmc-004.vm.brightbox.net"
+
+## List of servers
+role :app, "unepwcmc-004.vm.brightbox.net"
+role :web, "unepwcmc-004.vm.brightbox.net"
+role :db, 'unepwcmc-004.vm.brightbox.net', :primary => true
+# Target directory for the application on the web and app servers.
+set(:deploy_to) { File.join("", "home", user, application) }
+
+# URL of your source repository. By default this will just upload
+# the local directory.  You should probably change this if you use
+# another repository, like git or subversion.
+#set :deploy_via, :copy
+set :repository, "git@github.com:unepwcmc/mypolygon.git"
+
+set :scm, :git
+set :branch, "master"
+set :scm_username, "unepwcmc-read"
+set :scm_password, "conservation1"
+#set :deploy_via, :copy
+
+### Other options you can set ##
+# Comma separated list of additional domains for Apache
+# set :domain_aliases, "www.example.com,dev.example.com"
+
+## Local Shared Area
+# These are the list of files and directories that you want
+# to share between the releases of your application on a particular
+# server. It uses the same shared area as the log files.
+#
+# NOTE: local areas trump global areas, allowing you to have some
+# servers using local assets if required.
+#
+# So if you have an 'upload' directory in public, add 'public/upload'
+# to the :local_shared_dirs array.
+# If you want to share the database.yml add 'config/database.yml'
+# to the :local_shared_files array.
+#
+# The shared area is prepared with 'deploy:setup' and all the shared
+# items are symlinked in when the code is updated.
+# set :local_shared_dirs, %w(public/upload)
+# set :local_shared_files, %w(config/database.yml)
+set :global_shared_dirs, %w(public/system)
+set :global_shared_files, %w(config/database.yml)
+
+# SSH options. The forward agent option is used so that loopback logins
+# with keys work properly
+# ssh_options[:forward_agent] = true
+
+# Forces a Pty so that svn+ssh repository access will work. You
+# don't need this if you are using a different SCM system. Note that
+# ptys stop shell startup scripts from running.
+default_run_options[:pty] = true
+
+## Logrotation
+# Where the logs are stored. Defaults to <shared_path>/log
+# set :log_dir, "central/log/path"
+# The size at which to rotate a log. e.g 1G, 100M, 5M. Defaults to 100M
+# set :log_max_size, "100M"
+# How many old compressed logs to keep. Defaults to 10
+# set :log_keep, "10"
+
+task :setup_database_configuration do
+  the_host = Capistrano::CLI.ui.ask("Database IP address: ")
+  database_name = Capistrano::CLI.ui.ask("Database name: ")
+  database_user = Capistrano::CLI.ui.ask("Database username: ")
+  pg_password = Capistrano::CLI.password_prompt("Database user password: ")
+  require 'yaml'
+  spec = { "production" => {
+                                   "adapter" => "postgresql",
+                                   "database" => database_name,
+                                   "username" => database_user,
+                                   "host" => the_host,
+                                   "password" => pg_password }}
+  run "mkdir -p #{shared_path}/config"
+  put(spec.to_yaml, "#{shared_path}/config/database.yml")
+end
+after "deploy:setup", :setup_database_configuration
