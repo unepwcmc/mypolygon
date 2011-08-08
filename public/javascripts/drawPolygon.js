35880c14e79b31de76b7ee0aaef5f32ae072e1b4
James Cox <th3james@gmail.com>
2011-08-08 16:33:54 +0100

    Added Poly JS class, code now supports multiple polygons
    Squashed commit of the following:
    
    commit 2638f2a03cb338c21e1c027e7f3f6f786f0ee9f8
    Author: James Cox <th3james@gmail.com>
    Date:   Mon Aug 8 16:30:48 2011 +0100
    
        Fixed polygon drawing behavior
    
    commit 2d251e32783102be4ade6242723e1813b529eeb5
    Author: James Cox <th3james@gmail.com>
    Date:   Mon Aug 8 16:08:53 2011 +0100
    
        Fixed drag behavior
    
    commit bf0eae1649de7b07d3c6b16a18f1cdc143d45507
    Author: James Cox <th3james@gmail.com>
    Date:   Mon Aug 8 15:26:10 2011 +0100
    
        Fixed polysToGeoJSON method
    
    commit f5b52b93fa7829447a7fddebb869db08557ce397
    Author: James Cox <th3james@gmail.com>
    Date:   Mon Aug 8 14:03:46 2011 +0100
    
        Added Poly class, multipolygons draw, do not added correctly

--- public/javascripts/drawPolygon.js
+++ public/javascripts/drawPolygon.js
@@ -1,7 +1,94 @@
 /*drawing polygon*/
-var poly;
-var markers = [];
-var path = new google.maps.MVCArray;
+var allPolys = []; // Array of all the Polys on the map
+
+// Represents a polygon with its markers and path
+var Poly = (function() {
+  // Constructor -
+  // Creates and initialises a new google.maps.Polygon 
+  function Poly(polygon, markers, path) {
+    this.markers = [];
+    this.path = new google.maps.MVCArray;
+
+    this.polygon = new google.maps.Polygon({
+        strokeWeight: 2,
+        fillColor: '#FF6600',
+        strokeColor: '#FF6600'
+      });
+    this.polygon.setMap(map);
+    this.polygon.setPaths(new google.maps.MVCArray([this.path]));
+
+    //Tie click to add marker event
+    if (allPolys.length === 0){ //Only tie the event once
+      google.maps.event.addListener(map, 'click', function (event){
+        //Note: this method currently just adds to the last poly, not THIS poly
+        poly = allPolys[allPolys.length - 1];
+        poly.addPointUsingLatLng(event.latLng);
+      });
+    }
+
+    allPolys.push(this); // Add to the allPolys collection
+  }
+
+  // Add a point to the polygon using a latLng
+  // @param latLng
+  Poly.prototype.addPointUsingLatLng = function (latLng) {
+    this.path.insertAt(this.path.length, latLng);
+
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      draggable: true,
+      icon: vertexIcon,
+      poly: this
+    });
+    this.markers.push(marker);
+    marker.setTitle("#" + this.path.length);
+
+    google.maps.event.addListener(marker, 'click', function() {
+      poly = marker.poly;
+      if (poly.markers.length < 4) {
+        if (!$('#done').hasClass('disabled')) {
+            $('#done').addClass('disabled');
+        }
+      }
+
+      marker.setMap(null);
+      for (var i = 0, I = poly.markers.length; i < I && poly.markers[i] != marker; ++i);
+      poly.markers.splice(i, 1);
+      poly.path.removeAt(i);
+      }
+    );
+
+
+    google.maps.event.addListener(marker, 'dragend', function() {
+      for (var i = 0, I = marker.poly.markers.length; i < I && marker.poly.markers[i] != marker; ++i);
+      marker.poly.path.setAt(i, marker.getPosition());
+      }
+    );
+
+    if (this.markers.length > 2) {//do we have a polygon?
+      if ($('#done').hasClass('disabled')) {
+        $('#done').removeClass('disabled');
+      }
+    }
+  }
+
+  // Returns an JSON point array
+  Poly.prototype.toPointArray = function () {
+    var pathArray=[];
+    var _i, numPoints;
+    for(var _i=0, numPoints = this.path.length; _i < numPoints; _i++) {
+      var lat = this.path.getAt(_i).lat();
+      var lng = this.path.getAt(_i).lng();
+      pathArray.push([lng,lat]);
+    }
+    pathArray.push([this.path.getAt(0).lng(),this.path.getAt(0).lat()]); //google maps will automatically close the polygon; postgis requires the last coordinate to be repeted
+    return pathArray;
+  }
+
+  return Poly;
+})();
+
 
 var vertexIcon = new google.maps.MarkerImage('/images/sites/delete_vertex_noover.png',
     new google.maps.Size(12, 12),
@@ -34,16 +121,17 @@ function addPolygonToMap(points, repopulating) {
   }
 
   var myPolyCoords = [];
-  for(var i =0; i<points.length; i++) {
-    myPolyCoords[i] = new google.maps.LatLng(points[i][1], points[i][0]); // First 1, then 0 ?!?
+  var _i, _len;
+  for(_i = 0, _len = points.length; _i<_len; _i++) {
+    myPolyCoords[_i] = new google.maps.LatLng(points[_i][1], points[_i][0]); // First 1, then 0 ?!?
   }
 
   var bounds = new google.maps.LatLngBounds();
 
-  startPolygon();
-  for(i=0; i<myPolyCoords.length - 1; i++) {
-    addPointUsingLatLong(myPolyCoords[i]);
-    bounds.extend(myPolyCoords[i]);
+  var p = new Poly();
+  for(_i=0, _len = myPolyCoords.length - 1; _i<_len; _i++) {
+    p.addPointUsingLatLng(myPolyCoords[_i]);
+    bounds.extend(myPolyCoords[_i]);
   }
 
   map.fitBounds(bounds);
@@ -53,122 +141,66 @@ function addPolygonToMap(points, repopulating) {
 
 
 function addPoint(event) {
+  alert("refactor this method to use the addPointUsingLatLong method on a Poly object");
   addPointUsingLatLong(event.latLng)
 }
-function addPointUsingLatLong(latLng) {
-  path.insertAt(path.length, latLng);
 
-  var marker = new google.maps.Marker({
-    position: latLng,
-    map: map,
-    draggable: true,
-    icon: vertexIcon
-  });
-  markers.push(marker);
-  marker.setTitle("#" + path.length);
+/**
+ * Converts an array of Polys to GeoJSON
+ * @param polys An array of Polys
+ */
+function polys2geoJson(polys) {
+  var geojson={"type":"MultiPolygon"};
+  var json_polys = [];
+  var _i, _len;
+  for (_i=0, _len = polys.length; _i<_len; _i++) {
+    var poly = polys[_i];
 
-  google.maps.event.addListener(marker, 'click', function() {
-      if (markers.length < 4)
-      {
-        if (!$('#done').hasClass('disabled')) {
-            $('#done').addClass('disabled');
-        }
-      }
-
-    marker.setMap(null);
-    for (var i = 0, I = markers.length; i < I && markers[i] != marker; ++i);
-    markers.splice(i, 1);
-    path.removeAt(i);
-    }
-
-  );
-
-
-  google.maps.event.addListener(marker, 'dragend', function() {
-    for (var i = 0, I = markers.length; i < I && markers[i] != marker; ++i);
-    path.setAt(i, marker.getPosition());
-    }
-  );
-
-  if (markers.length > 2) //do we have a polygon?
-  {
-    if ($('#done').hasClass('disabled')) {
-		$('#done').removeClass('disabled');
-	}
+    json_polys.push([poly.toPointArray()]);
   }
-}
+  geojson['coordinates'] = json_polys;
 
-function startPolygon()  //destroy old polygon?
-{
-  poly = new google.maps.Polygon({
-    strokeWeight: 2,
-    fillColor: '#FF6600',
-    strokeColor: '#FF6600'
-  });
-  poly.setMap(map);
-  poly.setPaths(new google.maps.MVCArray([path]));
-
-  google.maps.event.addListener(map, 'click', addPoint);
-}
-
-/*adapted from Lifeweb's calculator.js*/
-function polys2geoJson(polygons) {
-    var geojson={"type":"MultiPolygon"};
-    var polys = [];
-    for (var i=0; i<polygons.length; i++)
-    {
-        var pol = polygons[i];
-        var polyArray =[];
-        var pathArray=[];
-        var numPoints = path.length;
-        for(var i=0; i < numPoints; i++) {
-            var lat = path.getAt(i).lat();
-            var lng = path.getAt(i).lng();
-            pathArray.push([lng,lat]);
-        }
-        pathArray.push([path.getAt(0).lng(),path.getAt(0).lat()]); //google maps will automatically close the polygon; postgis requires the last coordinate to be repeted
-        polyArray.push(pathArray);
-        polys.push(polyArray);
-    }
-    geojson['coordinates'] = polys;
-
-    return $.toJSON(geojson);
+  return $.toJSON(geojson);
 }
 
 function submitPolygon(){
   $('#done').addClass('loading');
-	var geojson = polys2geoJson([poly]);
+  var geojson = polys2geoJson(allPolys);
   var sources = [];
   $('#layers input:checkbox:checked').each(function() {
     sources.push($(this).val());
   });
 
-  area = google.maps.geometry.spherical.computeArea(poly.getPath());
-  if(area > MAX_POLYGON_AREA_KM2 * 1000 * 1000)
-  {
-    alert("Your polygon is too large ("+Math.round(area/1000/1000)+" km2), please limit its area to "+MAX_POLYGON_AREA_KM2+" km2.");
-    $('#done').removeClass('loading');
-    return false;
+  var area, poly, _i, _len;
+  for (_i = 0, _len = allPolys.length; _i < _len; _i++) {
+    polygon = allPolys[_i].polygon;
+    
+    area = google.maps.geometry.spherical.computeArea(polygon.getPath());
+    if(area > MAX_POLYGON_AREA_KM2 * 1000 * 1000) {
+      alert("Polygon is too large ("+Math.round(area/1000/1000)+" km2), please limit its area to "+MAX_POLYGON_AREA_KM2+" km2.");
+      $('#done').removeClass('loading');
+      return false;
+    }
   }
 
-	var dataObj = {"data": geojson, "sources": sources};
-	$.ajax({
-      type: 'POST',
-  		url: "assesments/createFromPolygon",
-  		data: dataObj,
-  		cache: false,
-			dataType: 'json',
-  		success: function(result){
-        if(typeof(result.assesment) != "undefined") {
-          window.location = 'assesments/' + result.assesment.id;
-        } else {
-          alert( result.error ||"Unknown error while uploading polygon.\nIs the polygon too big?\nOr perhaps its edges intersect each-other?" );
-        }
-        $('#done').removeClass('loading');
-  		},
-    	error:function (xhr, ajaxOptions, thrownError){
-        alert(thrownError);
-        $('#done').removeClass('loading');
-     	}
-		});
+  var dataObj = {"data": geojson, "sources": sources};
+  $.ajax({
+    type: 'POST',
+    url: "assesments/createFromPolygon",
+    data: dataObj,
+    cache: false,
+    dataType: 'json',
+    success: function(result){
+      if(typeof(result.assesment) != "undefined") {
+        window.location = 'assesments/' + result.assesment.id;
+      } else {
+        alert( result.error ||"Unknown error while uploading polygon.\nIs the polygon too big?\nOr perhaps its edges intersect each-other?" );
+      }
+      $('#done').removeClass('loading');
+    },
+    error:function (xhr, ajaxOptions, thrownError){
+      alert(thrownError);
+      $('#done').removeClass('loading');
+    }
+  });
 }
